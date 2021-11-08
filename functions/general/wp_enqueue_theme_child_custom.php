b8e73f2132f8d65ce892d8560c9e45fc7773440d
jonathan.alcaras@lecyclopeduweb.fr <jonathan.alcaras@lecyclopeduweb.fr>
2021-11-08 11:43:35 +0100

    fix bug

--- functions/general/wp_enqueue_theme_child_custom.php
+++ functions/general/wp_enqueue_theme_child_custom.php
@@ -2,6 +2,6 @@
 function wp_enqueue_theme_child_custom()
 {
 	//wp_enqueue_style('theme-child-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', false, '1.0', 'all');
-	//wp_enqueue_script('theme-child-script', get_stylesheet_directory_uri()() . '/js/theme.min.js', array(), '1.0.0', true);	wp_enqueue_script('theme-child-script');
+	//wp_enqueue_script('theme-child-script', get_stylesheet_directory_uri() . '/js/theme.min.js', array(), '1.0.0', true);	wp_enqueue_script('theme-child-script');
 }
 add_action('wp_enqueue_scripts', 'wp_enqueue_theme_child_custom');
